fe2312b2e40eb60281fab8e85b49b4dd81f7b2ec
Muh Zainun Hilmi <86088114+Miboyy@users.noreply.github.com>
2024-10-22 13:34:51 +0700

    Add files via upload

---
+++ Hilmi.py
@@ -0,0 +1,8 @@
+class montor:
+    nama = "supra Bapak"
+    roda = 2
+montorracing = montor()
+montorracing.roda = 3
+montorracing.nama ="Montor Liar"
+
+print(f"Nama: {montorracing.nama}, roda: {montorracing.roda}")
